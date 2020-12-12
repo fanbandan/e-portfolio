8f2dfcda40e1dd4b831253736389f10af6c08864
= <=>
2020-12-13 00:02:28 +1100

    Added a 404 page

---
+++ src/views/404.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="section">
+    <v-container fluid class="fill-height align-self-center">
+      <v-row align="center" justify="center">
+        <v-col cols="12" sm="8" md="4">
+          <v-sheet>
+            <h1>404 - Not Found</h1>
+            <h2 class="subtitle-1">
+              Hmmmm this isn't right, the page you are looking for seems to be
+              missing
+            </h2>
+            <v-btn large color="primary" link to="/" class="my-4">Home</v-btn>
+          </v-sheet>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "404",
+  components: {},
+};
+</script>
