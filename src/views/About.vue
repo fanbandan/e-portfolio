186d44340b6631bbd6899c47a448bc5d0d262c78
= <=>
2020-12-13 18:11:13 +1100

    Added content to about and projects

--- src/views/About.vue
+++ src/views/About.vue
@@ -7,16 +7,76 @@
           <v-card>
             <v-card-title> Passion for engineering </v-card-title>
             <v-card-text> Photos of my childhood robot </v-card-text>
+            <v-img
+              :lazy-src="require('../assets/hobbies/childhood-robot.jpg')"
+              :src="require('../assets/hobbies/childhood-robot.jpg')"
+              min-height="300"
+              alt="Mount Kuring-gai RFS Premier's Citation group photo"
+            >
+              <div class="fill-height bottom-gradient"></div>
+            </v-img>
           </v-card>
         </v-col>
         <v-col cols="12" md="6">
           <v-card>
             <v-card-title> Career Goals </v-card-title>
-            <v-card-subtitle>Where I am heading</v-card-subtitle>
+            <v-card-subtitle> My goals for the future </v-card-subtitle>
             <v-card-text>
-              <div>1-2 Years</div>
-              <div>5-10 Years</div>
+              <p>
+                In the short term, I’m aiming to find a graduate position in the
+                Defence Industry or working with Infrastructure after completing
+                my university degree.
+              </p>
+              <p>
+                I’d also like to return to Malaysia as part of my New Colombo
+                Plan program, to gain experience living and working in another
+                culture. It would be a great opportunity to further my
+                intercultural skills and understanding. With Asia set to host
+                some of the biggest economies of the 21st century, I believe
+                having a strong understanding of the culture and connections
+                throughout the region will be invaluable.
+              </p>
+              <p>
+                Long term, I see myself in a management style role. I believe
+                this type of role would be rewarding, and lead to a career where
+                I can embrace lifelong learning principals and practice
+                leadership. I am actively working on developing the skillset for
+                management
+              </p>
             </v-card-text>
+            <v-divider></v-divider>
+            <v-card-title class="">Areas of Development</v-card-title>
+            <v-card-subtitle>
+              Skills I am actively working on improving
+            </v-card-subtitle>
+            <v-card-text>
+              <div v-for="(skill, i) in skills" :key="i">
+                <template>
+                  <div class="py-2">
+                    <div v-text="skill.skill" class="font-italic mb-2"></div>
+                    <v-progress-linear
+                      :color="skill.colour"
+                      buffer-value="0"
+                      :value="skill.progress"
+                      rounded
+                      stream
+                    ></v-progress-linear>
+                  </div>
+                </template>
+              </div>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn
+                large
+                rounded
+                link
+                to="/skills"
+                color="primary"
+                class="px-4"
+              >
+                My Skills
+              </v-btn>
+            </v-card-actions>
           </v-card>
         </v-col>
         <v-col cols="12" md="6">
@@ -33,6 +93,50 @@
             <v-divisor></v-divisor>
             <v-card-subtitle>Programming</v-card-subtitle>
             <v-card-text>Carasel of past programming projects</v-card-text>
+            <v-carousel
+              cycle
+              height="400"
+              hide-delimiter-background
+              :show-arrows="true"
+              class="white--text align-end"
+            >
+              <v-carousel-item>
+                <v-img
+                  :lazy-src="require('../assets/hobbies/3d-printing.jpg')"
+                  :src="require('../assets/hobbies/3d-printing.jpg')"
+                  height="400"
+                  alt="3D printed gear mechanism"
+                  title="3D printed gear mechanism"
+                >
+                  <div class="fill-height bottom-gradient"></div>
+                </v-img>
+              </v-carousel-item>
+              <v-carousel-item>
+                <v-img
+                  :lazy-src="require('../assets/hobbies/go-kart.jpg')"
+                  :src="require('../assets/hobbies/go-kart.jpg')"
+                  height="400"
+                  alt="Self made go-kart"
+                  title="Self made go-kart"
+                >
+                  <div class="fill-height bottom-gradient"></div>
+                </v-img>
+              </v-carousel-item>
+              <v-carousel-item>
+                <v-img
+                  :lazy-src="require('../assets/hobbies/rfs.jpg')"
+                  :src="require('../assets/hobbies/rfs.jpg')"
+                  height="400"
+                  alt="Volunteering for the Rural Fire Service"
+                  title="Volunteering for the Rural Fire Service"
+                >
+                  <div class="fill-height bottom-gradient"></div>
+                </v-img>
+              </v-carousel-item>
+            </v-carousel>
+            <v-card-actions>
+              <v-btn text to="/projects" color="primary"> My Projects </v-btn>
+            </v-card-actions>
           </v-card>
         </v-col>
       </v-row>
@@ -44,5 +148,34 @@
 export default {
   name: "About",
   components: {},
+  data: () => ({
+    skills: [
+      {
+        skill: "Forecasting and planning",
+        progress: 40,
+        colour: "indigo",
+      },
+      {
+        skill: "Engaging with relevant community and stakeholders",
+        progress: 70,
+        colour: "amber",
+      },
+      {
+        skill: "Contracts and scope of works",
+        progress: 50,
+        colour: "deep-orange",
+      },
+      {
+        skill: "Practice working with legal and regulatory requirements",
+        progress: 10,
+        colour: "teal",
+      },
+      {
+        skill: "Contributing to proposals, bids, and tender documents",
+        progress: 20,
+        colour: "cyan",
+      },
+    ],
+  }),
 };
 </script>
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -3,53 +3,341 @@
     <v-container class="mt-8">
       <h1>Projects</h1>
       <h2 id="academic">Academic Projects</h2>
-      <v-row>
+      <v-row v-masonry>
         <v-col cols="12" md="6">
           <v-card>
-            <v-card-title> MX1 Unleashed </v-card-title>
-            <v-card-text> Video + Bio + GITHUB link </v-card-text>
+            <v-card-title> Mechactronics Unleashed </v-card-title>
+            <v-card-subtitle> 48622 Mechatronics 1 </v-card-subtitle>
+            <v-carousel
+              height="400"
+              hide-delimiter-background
+              :show-arrows="true"
+              class="white--text align-end"
+            >
+              <v-carousel-item>
+                <iframe
+                  width="100%"
+                  height="400"
+                  src="https://www.youtube.com/embed/pjwOmKsC7eE"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen
+                >
+                </iframe>
+              </v-carousel-item>
+              <v-carousel-item>
+                <v-img
+                  :lazy-src="require('../assets/projects/mx1-fpga-1.jpg')"
+                  :src="require('../assets/projects/mx1-fpga-1.jpg')"
+                  height="400"
+                  alt="FPGA Programming"
+                  title="FPGA Programming"
+                >
+                  <div class="fill-height bottom-gradient"></div>
+                </v-img>
+              </v-carousel-item>
+              <v-carousel-item>
+                <v-img
+                  :lazy-src="require('../assets/projects/mx1-fpga-2.jpg')"
+                  :src="require('../assets/projects/mx1-fpga-2.jpg')"
+                  height="400"
+                  alt="Design Sketch"
+                  title="Design Sketch"
+                >
+                  <div class="fill-height bottom-gradient"></div>
+                </v-img>
+              </v-carousel-item>
+            </v-carousel>
+            <v-card-text>
+              Selective studio program for high performing students</v-card-text
+            >
           </v-card>
         </v-col>
         <v-col cols="12" md="6">
           <v-card>
-            <v-card-title> MX2 Laser Robots </v-card-title>
-            <v-card-text> Picture + VIDEO + GITHUB link </v-card-text>
+            <v-card-title> Laser Tag Robots </v-card-title>
+            <v-card-subtitle> 48623 Mechatronics 2 </v-card-subtitle>
+            <iframe
+              width="100%"
+              height="400"
+              src="https://www.youtube.com/embed/gbckllF_aLk"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            >
+            </iframe>
+            <v-card-text></v-card-text>
+            <v-card-actions>
+              <v-btn
+                color="orange"
+                text
+                href="https://github.com/HimavaanChandra/laserRobots"
+              >
+                Project Github
+              </v-btn>
+            </v-card-actions>
           </v-card>
         </v-col>
         <v-col cols="12" md="6">
           <v-card>
-            <v-card-title> Warnman Project </v-card-title>
+            <v-card-title> Warman Design and Build Project </v-card-title>
+            <v-card-subtitle> 48600 Mechanical Design 1 </v-card-subtitle>
+            <v-carousel
+              cycle
+              height="400"
+              hide-delimiter-background
+              :show-arrows="true"
+              class="white--text align-end"
+            >
+              <v-carousel-item>
+                <v-img
+                  :lazy-src="require('../assets/projects/md1-project-1.jpg')"
+                  :src="require('../assets/projects/md1-project-1.jpg')"
+                  height="400"
+                  alt="Our Warman design"
+                  title="Our Warman design"
+                >
+                  <div class="fill-height bottom-gradient"></div>
+                </v-img>
+              </v-carousel-item>
+              <v-carousel-item>
+                <v-img
+                  :lazy-src="require('../assets/projects/md1-project-2.jpg')"
+                  :src="require('../assets/projects/md1-project-2.jpg')"
+                  height="400"
+                  alt="The Warman course"
+                  title="The Warman course"
+                >
+                  <!-- <div class="h4 font-weight-bold text-center py-2">
+                    The Warman course
+                  </div> -->
+                </v-img>
+              </v-carousel-item>
+              <v-carousel-item>
+                <v-img
+                  :lazy-src="require('../assets/projects/md1-project-4.jpg')"
+                  :src="require('../assets/projects/md1-project-4.jpg')"
+                  height="400"
+                  alt="Inital concept"
+                  title="Inital concept"
+                >
+                  <div class="fill-height bottom-gradient"></div>
+                </v-img>
+              </v-carousel-item>
+              <v-carousel-item>
+                <v-img
+                  :lazy-src="require('../assets/projects/md1-project-3.jpg')"
+                  :src="require('../assets/projects/md1-project-3.jpg')"
+                  height="400"
+                  alt="Base of robot"
+                  title="Base of robot"
+                >
+                  <div class="fill-height bottom-gradient"></div>
+                </v-img>
+              </v-carousel-item>
+            </v-carousel>
             <v-card-text> Picture + Bio </v-card-text>
           </v-card>
         </v-col>
         <v-col cols="12" md="6">
           <v-card>
-            <v-card-title> PFMS Project </v-card-title>
+            <v-card-title>
+              Probabilistic Road Map Path Finding Project
+            </v-card-title>
+            <v-card-subtitle>
+              41012 Programming for Mechatronic Systems
+            </v-card-subtitle>
+            <iframe
+              width="100%"
+              height="400"
+              :src="require('../assets/projects/pfms-prm.mp4')"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            >
+            </iframe>
             <v-card-text> Picture + GITHUB link </v-card-text>
+            <v-card-actions>
+              <v-btn
+                color="orange"
+                text
+                href="https://github.com/41012/pfms-2020a-fanbandan/tree/master/scratch/assignments/a4"
+              >
+                Project Github
+              </v-btn>
+            </v-card-actions>
           </v-card>
         </v-col>
         <v-col cols="12" md="6">
           <v-card>
-            <v-card-title> SCMS Project </v-card-title>
+            <v-card-title> Bar Tending Robot Project </v-card-title>
+            <v-card-subtitle> 41014 Sensors and Control </v-card-subtitle>
+            <iframe
+              width="100%"
+              height="400"
+              src="https://www.youtube.com/embed/j0EaATRQ0Yw"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            >
+            </iframe>
             <v-card-text> Video + GITHUB link </v-card-text>
+            <v-card-actions>
+              <v-btn
+                color="orange"
+                text
+                href="https://github.com/thomasluke/scms-fetch-robot"
+              >
+                Project Github
+              </v-btn>
+            </v-card-actions>
           </v-card>
         </v-col>
       </v-row>
       <h2 id="professional">Professional Projects</h2>
-      <v-row>
+      <v-row v-masonry>
         <v-col cols="12" md="6">
           <v-card>
-            <v-card-title> Websites </v-card-title>
+            <v-card-title> WordPress Website Design </v-card-title>
+            <v-card-subtitle> Web Contracting </v-card-subtitle>
+            <v-carousel
+              cycle
+              height="400"
+              hide-delimiter-background
+              :show-arrows="true"
+              class="white--text align-end"
+            >
+              <v-carousel-item>
+                <v-img
+                  :lazy-src="require('../assets/projects/web-contract-1.jpg')"
+                  :src="require('../assets/projects/web-contract-1.jpg')"
+                  height="400"
+                  alt="Ethos Aged Care"
+                  title="Ethos Aged Care"
+                >
+                  <div class="fill-height bottom-gradient"></div>
+                </v-img>
+              </v-carousel-item>
+              <v-carousel-item>
+                <v-img
+                  :lazy-src="require('../assets/projects/web-contract-2.jpg')"
+                  :src="require('../assets/projects/web-contract-2.jpg')"
+                  height="400"
+                  alt="InXite"
+                  title="InXite"
+                >
+                  <div class="fill-height bottom-gradient"></div>
+                </v-img>
+              </v-carousel-item>
+              <v-carousel-item>
+                <v-img
+                  :lazy-src="require('../assets/projects/web-contract-3.jpg')"
+                  :src="require('../assets/projects/web-contract-3.jpg')"
+                  height="400"
+                  alt="Sarah Muggeridge Mosaics"
+                  title="Sarah Muggeridge Mosaics"
+                >
+                  <div class="fill-height bottom-gradient"></div>
+                </v-img>
+              </v-carousel-item>
+            </v-carousel>
           </v-card>
         </v-col>
         <v-col cols="12" md="6">
           <v-card>
-            <v-card-title> Toshiba Project </v-card-title>
+            <v-card-title> Project Management Dashboard </v-card-title>
+            <v-card-subtitle>
+              Toshiba International Corporation
+            </v-card-subtitle>
+            <v-carousel
+              height="400"
+              hide-delimiter-background
+              :show-arrows="true"
+              class="white--text align-end"
+            >
+              <v-carousel-item>
+                <v-img
+                  :lazy-src="require('../assets/projects/tic-dashboard-1.jpg')"
+                  :src="require('../assets/projects/tic-dashboard-1.jpg')"
+                  height="400"
+                  alt="Dashboard sitemap"
+                  title="Dashboard sitemap"
+                >
+                  <div class="fill-height bottom-gradient"></div>
+                </v-img>
+              </v-carousel-item>
+              <v-carousel-item>
+                <v-img
+                  :lazy-src="require('../assets/projects/tic-dashboard-2.jpg')"
+                  :src="require('../assets/projects/tic-dashboard-2.jpg')"
+                  height="400"
+                  alt="Dashboard project overview"
+                  title="Dashboard project overview"
+                >
+                  <div class="fill-height bottom-gradient"></div>
+                </v-img>
+              </v-carousel-item>
+              <v-carousel-item>
+                <v-img
+                  :lazy-src="require('../assets/projects/tic-dashboard-3.jpg')"
+                  :src="require('../assets/projects/tic-dashboard-3.jpg')"
+                  height="400"
+                  alt="Dashboard project gantt chart"
+                  title="Dashboard project gantt chart"
+                >
+                  <div class="fill-height bottom-gradient"></div>
+                </v-img>
+              </v-carousel-item>
+              <v-carousel-item>
+                <v-img
+                  :lazy-src="require('../assets/projects/tic-dashboard-4.jpg')"
+                  :src="require('../assets/projects/tic-dashboard-4.jpg')"
+                  height="400"
+                  alt="Dashboard loading game"
+                  title="Dashboard loading game"
+                >
+                  <div class="fill-height bottom-gradient"></div>
+                </v-img>
+              </v-carousel-item>
+            </v-carousel>
           </v-card>
         </v-col>
         <v-col cols="12" md="6">
           <v-card>
-            <v-card-title> JEDS IMU logger </v-card-title>
+            <v-card-title> Naval Vessel Characterisation </v-card-title>
+            <v-card-subtitle>
+              Jenkins Engineering Defence Systems
+            </v-card-subtitle>
+            <v-carousel
+              cycle
+              height="400"
+              hide-delimiter-background
+              :show-arrows="true"
+              class="white--text align-end"
+            >
+              <v-carousel-item>
+                <v-img
+                  :lazy-src="require('../assets/projects/jeds-1.jpg')"
+                  :src="require('../assets/projects/jeds-1.jpg')"
+                  height="400"
+                  alt="Naval vessel characterisation device"
+                  title="Naval vessel characterisation device"
+                >
+                  <div class="fill-height bottom-gradient"></div>
+                </v-img>
+              </v-carousel-item>
+              <v-carousel-item>
+                <v-img
+                  :lazy-src="require('../assets/projects/jeds-2.jpg')"
+                  :src="require('../assets/projects/jeds-2.jpg')"
+                  height="400"
+                  alt="Electronic Warfare radar calculations"
+                  title="Electronic Warfare radar calculations"
+                >
+                  <div class="fill-height bottom-gradient"></div>
+                </v-img>
+              </v-carousel-item>
+            </v-carousel>
           </v-card>
         </v-col>
       </v-row>
@@ -61,5 +349,29 @@
 export default {
   name: "Projects",
   components: {},
+  mounted() {
+    this.repaint();
+  },
+  watch: {
+    items: function () {
+      this.repaint();
+    },
+  },
+  methods: {
+    repaint() {
+      setTimeout(() => this.$redrawVueMasonry(), 1500);
+    },
+  },
 };
 </script>
+
+<style lang="scss" scoped>
+@import "~vuetify/src/styles/styles.sass";
+.bottom-gradient {
+  background-image: linear-gradient(
+    to top,
+    rgba(0, 0, 0, 0.4) 0%,
+    transparent 72px
+  );
+}
+</style>
